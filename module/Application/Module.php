8beeec1caa977f181e83c29fefe32f6b56c720a5
Stefano Valle <s.valle@mvassociati.it>
2013-05-15 17:29:33 +0200

    Event manager used to catch framework dispatching errors

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -10,6 +10,7 @@
 namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\Application;
 use Zend\Mvc\MvcEvent;
 
 class Module
@@ -27,6 +28,31 @@ class Module
         $I_sm = $I_application->getServiceManager();
         $I_mailService = $I_sm->get('Application\Service\MailService');
         $I_sharedEventManager->attach('Events\Service\EventService', 'event_saved', array($I_mailService, 'logEventSaved'));
+        
+        // Common Error Handling Code
+        $I_eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, function($e) {
+        
+            // Do nothing if no error in the event
+            $error = $e->getError();
+            if (empty($error)) {
+                return;
+            }
+             
+            switch ($error) {
+                case Application::ERROR_CONTROLLER_NOT_FOUND:
+                case Application::ERROR_CONTROLLER_INVALID:
+                case Application::ERROR_ROUTER_NO_MATCH:
+                    // Specifically not handling these
+                    echo "Requested Resource Not Found";
+                    exit;
+                     
+                case Application::ERROR_EXCEPTION:
+                default:
+                    echo "Exception Thrown: " . $e->getParam('exception')->getMessage();
+                    exit;
+            }
+             
+        });
         
     }
 
